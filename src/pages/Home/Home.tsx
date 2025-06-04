f5890560c706b38bdad674c5a17a360d9b37baf5
saymamutovsaidakmol@gmail.com <116891087+saidkamol2006@users.noreply.github.com>
2025-06-04 17:37:58 +0500

    Search Paganation

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import { memo, useEffect, useRef, useState } from 'react'
-import useGetUser, { type responseT, type UserT } from './service/query/useGetUser';
+import useGetUser, { type UserT } from './service/query/useGetUser';
 import Form from './components/form';
 import User from './components/User';
 import Pagination from '@mui/material/Pagination';
@@ -11,7 +11,6 @@ const Home = () => {
 
   const [page, setPage] = useState(1)
   const { data, isLoading } = useGetUser(page)
-  const [dataRegion, setDataRegions] = useState<responseT | null>(null)
   // console.log(data?.regions?.totalPages);
 
   const handleChangePage = (event: React.ChangeEvent<unknown>, value: number) => {
@@ -20,16 +19,13 @@ const Home = () => {
   }
 
   useEffect(() => {
-    setDataRegions(data?.regions?.data);
+    console.log(data);
+
   }, [data])
 
   const handleSearch = () => {
     if (inputRef.current) {
-      // console.log(inputRef.current.value); // kiritilgan matn
-      const searchText = inputRef.current.value.toLowerCase();
-      const filtered = data?.regions?.data.filter((item) => item.name.toLowerCase().includes(searchText))
-      console.log(filtered);
-      setDataRegions(filtered)
+      console.log(inputRef.current.value); // kiritilgan matn
     }
 
   }
@@ -38,8 +34,8 @@ const Home = () => {
     <>
       <div className='container mx-auto mt-20 flex justify-center'>
         <div className=' flex items-center bg-[#eee] rounded-[5px]'>
-          <IoIosSearch onClick={handleSearch} className='text-[42px] px-2 cursor-pointer' />
-          <input className='outline-none' type="text" placeholder='search...' ref={inputRef} />
+          <IoIosSearch className='text-[42px] px-2 cursor-pointer' />
+          <input className='outline-none' type="text" placeholder='search...' />
           <button className=' h-full px-2 bg-blue-500 text-white  rounded-r-[5px] cursor-pointer' onClick={handleSearch}>Search</button>
         </div>
       </div>
@@ -50,14 +46,14 @@ const Home = () => {
             <div className='container mx-auto mt-20 grid grid-cols-4 gap-3'>
               {
                 //@ts-ignore
-                dataRegion?.map((item: UserT) => (
+                data?.data?.map((item: UserT) => (
                   <User key={item.id} {...item} />
                 ))
               }
             </div>
             <div className='container mx-auto  mt-10 flex justify-center'>
               <Stack spacing={2}>
-                <Pagination count={data?.regions.totalPages} onChange={handleChangePage} variant="outlined" />
+                <Pagination count={data?.totalPages} onChange={handleChangePage} variant="outlined" />
               </Stack>
             </div>
           </>
